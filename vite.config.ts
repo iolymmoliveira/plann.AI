ce374708061a4108623d02427eec9b715b37237a
ioly Oliveira <iolymmoliveira@gmail.com>
2026-06-25 16:32:40 -0300

    chore: update dependencies, Vite config and global styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
