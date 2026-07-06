2c89493279a994401c098d2695f1194bf8925aa8
ioly Oliveira <iolymmoliveira@gmail.com>
2026-07-06 11:37:46 -0300

    feat: implements navigation between form steps and input validation

--- src/components/features/Simulation/FormStep.tsx
+++ src/components/features/Simulation/FormStep.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/shared/Button'
 import { Input, type InputProps } from '@/components/shared/Input'
 import { ArrowLeft, ArrowRight, type LucideIcon } from 'lucide-react'
+import { useState, type SyntheticEvent } from 'react'
 
 export interface FormStepProps {
   id: string
@@ -14,13 +15,32 @@ export interface FormStepProps {
   }
 }
 
+interface ActionsButtonProps {
+  onBack: () => void
+  onNext: () => void
+  hideBackButton?: boolean
+}
+
 export function FormStep({
   icon: Icon,
   title,
   question,
   inputProps,
   submitButtonProps,
-}: FormStepProps) {
+  onBack,
+  onNext,
+  hideBackButton,
+}: FormStepProps & ActionsButtonProps) {
+  const [inputValue, setInputValue] = useState('')
+
+  function handleSubmit(e: SyntheticEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    if (!inputValue) return
+
+    onNext()
+  }
+
   return (
     <div className="bg-card rounded-2xl p-6 shadow-[4px_4px_18px_0px_rgba(0,0,0,0.02)] sm:p-8">
       <div className="bg-primary h-15 w-15 mb-4 flex items-center justify-center rounded-xl">
@@ -30,23 +50,26 @@ export function FormStep({
       <h3 className="text-foreground mb-6 text-xl font-semibold leading-snug sm:text-2xl">
         {question}
       </h3>
-      <form className="flex flex-col gap-4">
-        <Input {...inputProps} />
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <Input {...inputProps} value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
         <div className="flex flex-col gap-3 sm:flex-row sm:gap-6">
-          <Button
-            type="button"
-            icon={ArrowLeft}
-            // onClick={onBack}
-            variant="ghost"
-            className="order-2 flex-1 justify-center rounded-xl py-3 sm:order-1"
-          >
-            Voltar
-          </Button>
+          {!hideBackButton && (
+            <Button
+              type="button"
+              icon={ArrowLeft}
+              onClick={onBack}
+              variant="ghost"
+              className="order-2 flex-1 justify-center rounded-xl py-3 sm:order-1"
+            >
+              Voltar
+            </Button>
+          )}
           <Button
             type="submit"
             icon={!submitButtonProps ? ArrowRight : undefined}
             variant="primary"
             className="order-1 flex-1 sm:order-2"
+            disabled={!inputValue}
           >
             {submitButtonProps?.label ?? 'Próximo'}
             {submitButtonProps?.emojiIcon}
--- src/components/features/Simulation/SimulationForm.tsx
+++ src/components/features/Simulation/SimulationForm.tsx
@@ -1,14 +1,37 @@
 import { simulationFormSteps } from '@/data/simulation'
+import { useState } from 'react'
 import { FormStep } from './FormStep'
 import { Progress } from './Progress'
 
 export function SimulationForm() {
-  const currentStep = simulationFormSteps[0] // Example: using the first step for demonstration
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
+  const totalSteps = simulationFormSteps.length
+  const currentStep = simulationFormSteps[currentStepIndex]
+
+  const handleNextStep = () => {
+    if (currentStepIndex + 1 > totalSteps - 1) {
+      return
+    }
+    setCurrentStepIndex((prev) => prev + 1)
+  }
+
+  const handlePreviousStep = () => {
+    if (currentStepIndex === 0) {
+      return
+    }
+    setCurrentStepIndex((prev) => prev - 1)
+  }
 
   return (
     <>
-      <Progress currentStep={1} totalSteps={6} />
-      <FormStep key={currentStep.id} {...currentStep} />
+      <Progress currentStep={currentStepIndex + 1} totalSteps={totalSteps} />
+      <FormStep
+        key={currentStep.id}
+        {...currentStep}
+        onBack={handlePreviousStep}
+        onNext={handleNextStep}
+        hideBackButton={currentStepIndex === 0}
+      />
     </>
   )
 }
